8063694372fd93976d4a41e9950a9e742e61de07
Romualdo Dasig <dasig.rg@a5project.com>
2019-07-04 20:07:41 +0800

    Implement feature to add a song.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import uuid from 'uuid';
 import Header from './components/layout/Header';
 import Songs from './components/song/Songs';
+import AddSong from './components/song/AddSong';
 import RequestSong from './components/song/RequestSong';
 import About from './components/pages/About';
 import './App.css';
-import { toUnicode } from 'punycode';
 
 class App extends Component {
   state = {
     songs: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Girl",
         artist: "Immacualte",
         added: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "She's Gone",
         artist: "Steelhear",
         added: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Highway Star",
         artist: "Deep Purple",
         added: false
       },
       {
-        id: 4,
+        id: uuid.v4(),
         title: "Crazy",
         artist: "Aerosmith",
         added: true
       },
       {
-        id: 5,
+        id: uuid.v4(),
         title: "November Rain",
         artist: "Guns N' Roses",
         added: false
@@ -59,6 +60,17 @@ class App extends Component {
     this.setState({ songs: [...this.state.songs.filter(song => song.id !== id)] });
   }
 
+  // Add song.
+  addSong = (title) => {
+    const newSong = {
+      id: uuid.v4(),
+      title: title,
+      artist: "Max Surban",
+      added: false
+    }
+    this.setState({ songs: [...this.state.songs, newSong] });
+  }
+
   // Request a song.
   requestSong = (title) => {
     console.log(title);
@@ -73,10 +85,11 @@ class App extends Component {
 
             <Route exact path="/" render={props => (
               <React.Fragment>
-                <RequestSong requestSong={this.requestSong} />
+                <AddSong addSong={this.addSong} />
                 <div className="main">
                   <Songs songs={this.state.songs} markAddedToLibrary={this.markAddedToLibrary} delSong={this.delSong} />
                 </div>
+                <RequestSong requestSong={this.requestSong} />
               </React.Fragment>
             )} />
 
